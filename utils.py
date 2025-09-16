181d7220c3b1fd40d5a1a368af837f694c01770e
Niagui <niagui@Niagui.localdomain>
2025-09-15 18:17:14 -0700

    added interpolate

--- utils.py
+++ utils.py
@@ -1,6 +1,16 @@
 import librosa
 import soundfile as sf
+import json
+import np
 
 def getDurtion(audioFile):
     info = sf.info(audioFile)
     return info.frames / info.samplerate
+
+def saveAsJson(filename, data):
+    with open(f'json/{filename}.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4)
+
+def smoothstep(t):
+    t = np.clip(t, 0.0, 1.0)
+    return t*t*(3.0 - 2.0*t)
