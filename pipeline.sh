45a62f0503cc5ab507d919024fce48e53a8a8b5a
Niagui <niagui@Niagui.localdomain>
2025-11-13 05:15:53 -0800

    minor fix on fix

--- pipeline.sh
+++ pipeline.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-echo "[1/3] Running Python..."
+echo "[1/3] Running Clap Analysis..."
 python3 src/main.py       # writes data/output.json
 
-echo "[2/3] Running make..."
+echo "[2/3] Compiling Visualization Program..."
 make -C visuals
 
-echo "[3/3] Running C++ program..."
-./visuals/drones 
+echo "[3/3] Running Visualization Program..."
+./visuals/drones
